067c03f23b35a908d310651a0d5a54488f1cc59f
scurry222 <scurry222@gmail.com>
2020-12-09 17:17:09 -0800

    Add store

--- src/reducers/videoList.js
+++ src/reducers/videoList.js
@@ -1,13 +1,13 @@
 import Redux from 'redux';
 import {sampleVideoData as sampleData} from '../data/exampleVideoData.js';
 
-var videoListReducer = (state = sampleData, action) => {
+var videoListReducer = (state = [], action) => {
   //TODO: define a reducer for the videoList field of our state.
   switch (action.type) {
   case 'CHANGE_VIDEO_LIST':
     return action.videos;
   default:
-    return [];
+    return state;
   }
 };
 
--- src/store/store.js
+++ src/store/store.js
@@ -4,13 +4,13 @@ import rootReducer from './../reducers/main.js';
 import exampleVideoData from '../data/exampleVideoData.js';
 
 // TODO:  Create your redux store, apply thunk as a middleware, and export it!
-
-var configureStore = function(initialState) {
-  return createStore(
-    rootReducer,
-    initialState,
-    applyMiddleware(thunk)
-  );
-};
+var configureStore = createStore(
+  rootReducer,
+  {
+    currentVideo: exampleVideoData[0],
+    videoList: exampleVideoData,
+  },
+  applyMiddleware(thunk)
+);
 
 export default configureStore;
